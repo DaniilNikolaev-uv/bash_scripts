7d226afd13ca3c1c32a815bf766fbfd7bfecc4da
Daniil Nikolaev <lilwasd23@gmail.com>
2026-04-27 14:06:23 +0300

    fix: .zshrc script

--- server_install.sh
+++ server_install.sh
@@ -37,6 +37,10 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
 git clone https://github.com/zsh-users/zsh-autosuggestions.git \
   "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 
+rm .zshrc
+
+touch .zshrc
+
 cat <<'EOF' >>~/.zshrc
 
 export ZSH="$HOME/.oh-my-zsh"
@@ -56,4 +60,31 @@ source $ZSH/oh-my-zsh.sh
 
 EOF
 
-echo "Done!"
+echo "Done. Installing Docker"
+
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl enable docker
+
+sudo groupadd docker
+
+sudo usermod -aG docker "$USER"
